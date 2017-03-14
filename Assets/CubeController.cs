1affb35c76aeeac8d8afeff4cd776cf04fd1698e
大島彩華 <1515honoka@gmail.com>
2017-03-14 14:08:52 +0900

    レッスン7課題

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+    //
+    private AudioSource audiosource;
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+        //
+        audiosource = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //
+        if (coll.gameObject.tag == "ground" || coll.gameObject.tag == "Cube")
+        {
+            audiosource.Play();
+        }
+    }
+}
